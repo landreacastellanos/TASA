4580571b1bc71a6b18df6c4fcebc018a7e0e4693
jl <julis1997lg@gmail.com>
2020-07-16 12:23:05 -0500

    Get db variable from the env

--- backend/connection.py
+++ backend/connection.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import os
 
 def connection():
     db = mysql.connector.connect(
         host="localhost",
-        user="admin",
-        password="Admin123.",
-        database="tasa"
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     return db
 
