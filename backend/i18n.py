55219ee39324d7d9cfa96efb20923be100f79145
jl <julis1997lg@gmail.com>
2020-08-04 23:10:50 -0500

    add map to i18n translation

---
+++ backend/i18n.py
@@ -0,0 +1,9 @@
+def i18n(key):
+    i18n = {
+        "name": "Nombre",
+        "last_name": "Apellido",
+        "age": "Edad",
+        "profesion": "Profesión",
+        "email": "Correo Electronico",
+    }
+    return i18n[key]
