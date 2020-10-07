54190f09a8bd8d19374e9c77dc379ba095a0e5fe
jl <julis1997lg@gmail.com>
2020-10-07 17:34:58 -0500

    add property id and stage id in the function that update the sowing date

--- backend/main.py
+++ backend/main.py
@@ -228,7 +228,7 @@ def addStage():
         seedtime = request.form['seedtime']
         land_id = request.form['land_id']
         sowing_type = request.form['sowing_type']
-        result = funcs.updateSeedtimeByLandId(land_id, seedtime, variety_land, sowing_type)
+        result = funcs.updateSeedtimeByLandId(land_id, seedtime, variety_land, sowing_type, pid, stage_id)
         if result == -1:
             print("Error Reported")
             return "<script> alert('Ocurrio un Error');location.href='/';</script>"
