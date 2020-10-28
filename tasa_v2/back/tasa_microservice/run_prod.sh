07ebeee349a3d56a693e77efdd11a22f1a3053af
Leonardo Bustamante <diego.bustamante@fitideas.co>
2020-10-28 11:46:46 -0500

    add git ignore

--- tasa_v2/back/tasa_microservice/run_prod.sh
+++ tasa_v2/back/tasa_microservice/run_prod.sh
@@ -2,8 +2,8 @@ rm -rf env_development
 rm -rf __pycache__
 
 virtualenv -p /usr/bin/python3 env_development
+source env_development/bin/activate
 pip3 install -r ./requirements/base.txt
 
-source env_development/bin/activate
 export FLASK_APP=manage.py
 flask run --host=0.0.0.0 --port 5024
