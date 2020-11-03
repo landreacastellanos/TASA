e6d62fe079bf040aed3274d4ef0d93503c46c891
JuanSe Dussan <juanse.cloud@gmail.com>
2020-11-03 18:44:47 -0500

    feat(HU-4): back-integration

--- tasa_v2/front/src/app/pages/home/users/create/create.component.ts
+++ tasa_v2/front/src/app/pages/home/users/create/create.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Role } from '../../../../shared/models/role';
+import { RoleService } from '../../../../shared/services/role.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
+  constructor(
+    private _userService: UserService,
+    private _roleService: RoleService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  save(event) {
+    console.log(event);
+    const randNum = Math.floor(Math.random() * 1000);
+    return this._userService.create({
+      age: randNum,
+      email: `juanse_${randNum}@yopmail.com`,
+      last_name: `dussan_${randNum}`,
+      name: `juanse_${randNum}`,
+      password: `juanse_${randNum}@yopmail.com`,
+      phone: randNum,
+      profesion: `profesion_${randNum}`,
+      role_id: 6,
+    });
   }
 
+  get roles(): Promise<Role[]> {
+    return this._roleService.allRolesPromise;
+  }
 }
