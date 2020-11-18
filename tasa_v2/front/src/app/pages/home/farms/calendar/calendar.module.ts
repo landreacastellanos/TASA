c47d319c3393972d4a46ab927d670e0b04e5d20f
Vanessa <vanesora@gmail.com>
2020-11-18 17:08:10 -0500

    calendar component

--- tasa_v2/front/src/app/pages/home/farms/calendar/calendar.module.ts
+++ tasa_v2/front/src/app/pages/home/farms/calendar/calendar.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CreateComponent } from './create/create.component';
-import { FarmsRoutingModule } from './farms-routing.module';
-import { ListComponent } from './list/list.component';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -12,13 +9,14 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { CalendarComponent } from './calendar.component';
+import { CalendarRoutingModule } from './calendar-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    CreateComponent,
-    ListComponent,
+    CalendarComponent
   ],
   imports: [
     CommonModule,
@@ -33,7 +31,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatTableModule,
     MatPaginatorModule,
     MatCheckboxModule,
-    FarmsRoutingModule
+    CalendarRoutingModule
   ]
 })
-export class FarmsModule { }
+export class CalendarModule { }
