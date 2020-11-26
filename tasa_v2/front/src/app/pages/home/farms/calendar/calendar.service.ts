0b0020f3c12d16aa79b8071207a034ebb50674f8
JuanSe Dussan <juanse.cloud@gmail.com>
2020-11-25 21:15:48 -0500

    integrate photos

--- tasa_v2/front/src/app/pages/home/farms/calendar/calendar.service.ts
+++ tasa_v2/front/src/app/pages/home/farms/calendar/calendar.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   StageOneRequest,
   StageOneResponse,
+  UploadFileResponse,
 } from 'src/app/shared/models/calendar';
 import { LandResponse } from '../../../../shared/models/lands';
 import { DataApiService } from '../../../../shared/services/data-api.service';
@@ -13,30 +14,29 @@ export class CalendarService {
   constructor(private dataApiService: DataApiService) {}
 
   getStageOne(landId: string | number): Promise<StageOneResponse> {
-    return (
-      this.dataApiService
-        .getAll('get_stage_one?land_id=' + landId)
-        .then((dataResponse) => {
-          return dataResponse[0];
-        })
-        // FIXME: remove me please, only for test
-        // .then((dataResponse2) => {
-        //   return {
-        //     enabled: true,
-        //     real_date:
-        //       'Tue Nov 22 2020 00:23:08 GMT-0500 (hora estándar de Colombia)',
-        //     sowing_date:
-        //       'Tue Nov 21 2020 00:23:08 GMT-0500 (hora estándar de Colombia)',
-        //     type_sowing: 'Siembra con arroz tapado',
-        //     variety: 'Arroz de diosito',
-        //   };
-        // })
-    );
+    return this.dataApiService
+      .getAll('get_stage_one?land_id=' + landId)
+      .then((dataResponse) => {
+        return dataResponse[0];
+      });
   }
 
   setStageOne(data: StageOneRequest): Promise<string> {
     return this.dataApiService
       .post(data, 'set_stage_one')
+      .then((dataResponse) => {
+        return dataResponse[0];
+      });
+  }
+
+  uploadFiles(files: FileList): Promise<UploadFileResponse> {
+    const formData = new FormData();
+    for (let index = 0; index < files.length; index++) {
+      const element = files[index];
+      formData.append(`image_${index + 1}`, element);
+    }
+    return this.dataApiService
+      .postMultipart(formData, 'upload_file')
       .then((dataResponse) => {
         return dataResponse[0];
       });
--- tasa_v2/front/src/app/shared/models/calendar.ts
+++ tasa_v2/front/src/app/shared/models/calendar.ts
@@ -14,3 +14,7 @@ export interface StageOneRequest extends StageOne {
   images?: string[];
   land_id: number;
 }
+
+// tslint:disable-next-line: max-line-length
+/** Response array of string storage on server like `["1f035f78-2f80-11eb-a432-ded54758e1b2.jpg", "1f035f79-2f80-11eb-a432-ded54758e1b2.jpeg", "1f035f7a-2f80-11eb-a432-ded54758e1b2.jpg"]` */
+export type UploadFileResponse = string[];
