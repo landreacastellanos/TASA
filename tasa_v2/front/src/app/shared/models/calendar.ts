1cf82def8bd0b101ff49f0bb5bc3b03847cc479d
JuanSe Dussan <juanse.cloud@gmail.com>
2020-12-07 15:35:50 -0500

    Integrate APIS porducts, get stage one and View

--- tasa_v2/front/src/app/shared/models/calendar.ts
+++ tasa_v2/front/src/app/shared/models/calendar.ts
@@ -1,3 +1,16 @@
+export interface Products {
+  color: string;
+  commercial_name: string;
+  concentration: string;
+  dose_by_ha: number;
+  formulator: string;
+  id: number;
+  ing_active: string;
+  presentation: string;
+  provider: string;
+  segment: string;
+}
+
 export interface StageBetween {
   application_date: string;
   end_traking_date: string;
