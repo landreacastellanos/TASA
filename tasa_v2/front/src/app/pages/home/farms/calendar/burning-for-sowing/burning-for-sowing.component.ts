259a1a3321c257ba52fe709d3851ef639ae23c6e
JuanSe Dussan <juanse.cloud@gmail.com>
2020-12-11 20:47:55 -0500

    integrate set_stage

--- tasa_v2/front/src/app/pages/home/farms/calendar/burning-for-sowing/burning-for-sowing.component.ts
+++ tasa_v2/front/src/app/pages/home/farms/calendar/burning-for-sowing/burning-for-sowing.component.ts
@@ -286,7 +286,7 @@ export class BurningForSowingComponent implements OnInit, CalendarChildren {
         if (filesSaved) {
           dataRequest.images = filesSaved;
         }
-        return this.calendarService.setBurnStage(dataRequest);
+        return this.calendarService.setStage(dataRequest);
       })
       .then(
         (message) =>
--- tasa_v2/front/src/app/pages/home/farms/calendar/calendar.service.ts
+++ tasa_v2/front/src/app/pages/home/farms/calendar/calendar.service.ts
@@ -13,8 +13,7 @@ import { DataApiService } from '../../../../shared/services/data-api.service';
   providedIn: 'root',
 })
 export class CalendarService {
-
-  constructor(private dataApiService: DataApiService) { }
+  constructor(private dataApiService: DataApiService) {}
 
   getProducts(
     idLand: string | number,
@@ -39,35 +38,11 @@ export class CalendarService {
     stage: string | number,
     landId: string | number
   ): Promise<StageBetweenResponse> {
-    return (
-      this.dataApiService
-        .getAll(`get_stage?land_id=${landId}&stage_number=${stage}`)
-        .then((dataResponse) => {
-          return dataResponse[0];
-        })
-        // FIXME: Only for test
-        .then(() => ({
-          application_date: '2020-10-11T05:00:00.000Z',
-          enabled: true,
-          end_traking_date: '2020-10-20T05:00:00.000Z',
-          start_traking_date: '2020-10-28T05:00:00.000Z',
-          observations: 'Está como feo el arroz',
-          products: [
-            {
-              color: 'Verde',
-              commercial_name: 'Cosmo agua',
-              concentration: '0,05',
-              dose_by_ha: 1.9,
-              formulator: 'Solitec',
-              id: 110,
-              ing_active: 'Bacterias',
-              presentation: '1 y Lts',
-              provider: 'Solitec',
-              segment: 'Bactericida',
-            },
-          ],
-        }))
-    );
+    return this.dataApiService
+      .getAll(`get_stage?land_id=${landId}&stage_number=${stage}`)
+      .then((dataResponse) => {
+        return dataResponse[0];
+      });
   }
 
   setStageOne(data: StageOneRequest): Promise<string> {
@@ -78,15 +53,13 @@ export class CalendarService {
       });
   }
 
-  setBurnStage(data: StageBetweenRequest): Promise<string> {
-    console.debug('CalendarService:setBurnStage', { data });
-    return Promise.resolve('FIXME: API NOT INTEGRATED');
-    // FIXME: add correct implementation
-    // this.dataApiService
-    //   .post(data, '????')
-    //   .then((dataResponse) => {
-    //     return dataResponse[0];
-    //   });
+  setStage(data: StageBetweenRequest): Promise<string> {
+    console.debug('CalendarService:setStage', { data });
+    return this.dataApiService
+      .post(data, 'set_stage')
+      .then((dataResponse) => {
+        return dataResponse[0];
+      });
   }
 
   uploadFiles(files: FileList): Promise<UploadFileResponse> {
