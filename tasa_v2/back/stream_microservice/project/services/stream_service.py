23365a5b87645ee90ebed72d48b5c4c40e3f8668
Leonardo Bustamante <diego.bustamante@fitideas.co>
2021-02-16 15:17:39 -0500

    Se agregan cambios para el chat

--- tasa_v2/back/stream_microservice/project/services/stream_service.py
+++ tasa_v2/back/stream_microservice/project/services/stream_service.py
@@ -17,6 +17,12 @@ class NotificationService():
         self.__repository_user = CommonRepository(
          entity_name="user")
 
+        self.__repository_properties = CommonRepository(
+         entity_name="properties")
+
+        self.__repository_land = CommonRepository(
+         entity_name="land")   
+
     def get_notification(self):
 
         results = {
@@ -38,7 +44,7 @@ class NotificationService():
             results['data'].append(data)            
         return results
 
-    def delete_notification(self, data):
+    def delete_notification(self, type_id, data):
 
         results = {
             "data": [],
@@ -61,7 +67,9 @@ class NotificationService():
         })  
 
         for item in data:
-            self.__repository_notification.delete(item['id'],entity_name="notification")
+            notification = json.loads(item['Notification'])
+            if(notification['type'] == type_id):
+                self.__repository_notification.delete(item['id'],entity_name="notification")
 
         results['data'].append("Notificaciones eliminadas.")
         return results
@@ -107,7 +115,43 @@ class NotificationService():
         }
 
         self.__repository_chat.insert(chat)
-
+        self.set_chat_notification(data['id_land'], user['id'])
         results['data'].append("Chat creado correctamente.")
 
         return results
+
+    
+    def set_chat_notification(self, land_id, user_id):
+
+        land = self.__repository_land.select_one(land_id)[0]
+        property_field = self.__repository_properties.select_one(int(land["property_id"]))[0]
+        user = self.__repository_user.select_one(user_id)[0]
+
+        notification = {
+                "land_id": land['id'],
+                "type": 3, 
+                "batch_name": land['land_name'], 
+                "property_id": property_field['id'], 
+                "property_name": property_field['name'],
+                "id_user": user['id'],
+                "user_name": user['email'],
+        }        
+        if(property_field['manager'] is not None):
+            self.insert_data(notification, property_field['manager'])
+        if(property_field['property_owner'] is not None):
+            self.insert_data(notification, property_field['property_owner'])
+
+        if(property_field['parthner_add'] is not None):
+            self.insert_data(notification, property_field['parthner_add'])
+        if(property_field['seller'] is not None):
+            self.insert_data(notification, property_field['seller'])
+
+
+    def insert_data(self, notification, user_id):
+            user = self.__repository_user.select_one(user_id)[0]
+            user_notification = {
+                "id_user": user['id'],
+                "user_name": user['email'],
+                "Notification": json.dumps(notification)                
+            }
+            self.__repository_notification.insert(user_notification)
