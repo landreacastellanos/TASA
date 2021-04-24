1374075980bb8665d308012287e09a82872182bb
JuanSe Dussan <juanse.cloud@gmail.com>
2021-04-24 13:19:03 -0500

    Add migration bd

--- tasa_v2/infrastructure/vm/start_up.sh
+++ tasa_v2/infrastructure/vm/start_up.sh
@@ -14,3 +14,11 @@ cat /etc/fstab
 # Docker install
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+
+sudo curl -L "https://github.com/docker/compose/releases/
+download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+apt install git -y
+git clone https://github.com/landreacastellanos/TASA.git
+
