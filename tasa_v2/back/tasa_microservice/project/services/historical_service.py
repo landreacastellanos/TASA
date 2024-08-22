7745107a0ad8b6b89eb848c784eaed85d4c79a9d
andesleodiego24 <dl.bustamante@uniandes.edu.co>
2024-08-22 22:05:21 +0000

    se hace fix de reporte

--- tasa_v2/back/tasa_microservice/project/services/historical_service.py
+++ tasa_v2/back/tasa_microservice/project/services/historical_service.py
@@ -65,39 +65,40 @@ class HistoricalService:
                and dateutil.parser.parse(data['application_date']).year == year):
                 air_application = json.loads(item['air_application'])
                 current_element = list(filter(lambda x: x['batch_id'] == item['land_id'] and x['stage_id'] == item['stage_id'], response))
-                drone = self.__repository_drones.select_one(int(air_application['drone']))[0]
-                if len(current_element) == 0:
-                    response_data = {}
-                    land = self.__repository_land.select_one(int(item['land_id']))[0]
-                    property = self.__repository_properties.select_one(int(land["property_id"]))[0]
-                    stage = self.__repository_stage.select_one(int(item['stage_id']))[0]
-                    response_data['batch'] = land['land_name']
-                    response_data['batch_id'] = land['id']
-                    response_data['property'] = property['name']
-                    response_data['stage'] = stage['stage']
-                    response_data['stage_id'] = stage['id']
-                    response_data['date'] = data['application_date']
-                    response_data['drones'] = [{'dron': drone['name'],
-                                                'id': drone['id'],
-                                                'dead': str(datetime.strptime(air_application['deadTime'], '%H:%M').time()),
-                                                'live': str(datetime.strptime(air_application['liveTime'], '%H:%M').time()),
-                                                'ha': float(air_application['dropSize'])}]
-                    response.append(response_data)
-                else:
-                    dron = list(filter(lambda x: x['id'] == air_application['drone'], current_element[0]['drones']))
-                    if not dron:
-                        current_element[0]['drones'].append(
-                                                {'dron': drone['name'],
-                                                'id': drone['id'],
-                                                'dead': str(datetime.strptime(air_application['deadTime'], '%H:%M').time()),
-                                                'live': str(datetime.strptime(air_application['liveTime'], '%H:%M').time()),
-                                                'ha': float(air_application['dropSize'])})
+                if air_application['drone'] is not None and air_application['drone'] != '':
+                    drone = self.__repository_drones.select_one(int(air_application['drone']))[0]
+                    if len(current_element) == 0:
+                        response_data = {}
+                        land = self.__repository_land.select_one(int(item['land_id']))[0]
+                        property = self.__repository_properties.select_one(int(land["property_id"]))[0]
+                        stage = self.__repository_stage.select_one(int(item['stage_id']))[0]
+                        response_data['batch'] = land['land_name']
+                        response_data['batch_id'] = land['id']
+                        response_data['property'] = property['name']
+                        response_data['stage'] = stage['stage']
+                        response_data['stage_id'] = stage['id']
+                        response_data['date'] = data['application_date']
+                        response_data['drones'] = [{'dron': drone['name'],
+                                                    'id': drone['id'],
+                                                    'dead': str(datetime.strptime(air_application['deadTime'] if air_application['deadTime'] != '' else '00:00', '%H:%M').time()),
+                                                    'live': str(datetime.strptime(air_application['liveTime'] if air_application['liveTime'] != '' else '00:00', '%H:%M').time()),
+                                                    'ha': float(air_application['dropSize'])}]
+                        response.append(response_data)
                     else:
-                        dron[0]['ha'] += float(air_application['dropSize'])
-                        live = datetime.strptime(dron[0]['live'], '%H:%M:%S') 
-                        dead = datetime.strptime(dron[0]['dead'], '%H:%M:%S') 
-                        dron[0]['dead'] = str((datetime.strptime(air_application['deadTime'], '%H:%M') + timedelta(hours=dead.hour, minutes=dead.minute)).time())
-                        dron[0]['live'] = str((datetime.strptime(air_application['liveTime'], '%H:%M') + timedelta(hours=live.hour, minutes=live.minute)).time())
+                        dron = list(filter(lambda x: x['id'] == air_application['drone'], current_element[0]['drones']))
+                        if not dron:
+                            current_element[0]['drones'].append(
+                                                    {'dron': drone['name'],
+                                                    'id': drone['id'],
+                                                    'dead': str(datetime.strptime(air_application['deadTime'] if air_application['deadTime'] != '' else '00:00', '%H:%M').time()),
+                                                    'live': str(datetime.strptime(air_application['liveTime'] if air_application['liveTime'] != '' else '00:00', '%H:%M').time()),
+                                                    'ha': float(air_application['dropSize'])})
+                        else:
+                            dron[0]['ha'] += float(air_application['dropSize'])
+                            live = datetime.strptime(dron[0]['live'], '%H:%M:%S') 
+                            dead = datetime.strptime(dron[0]['dead'], '%H:%M:%S') 
+                            dron[0]['dead'] = str((datetime.strptime(air_application['deadTime'] if air_application['deadTime'] != '' else '00:00', '%H:%M') + timedelta(hours=dead.hour, minutes=dead.minute)).time())
+                            dron[0]['live'] = str((datetime.strptime(air_application['liveTime'] if air_application['liveTime'] != '' else '00:00', '%H:%M') + timedelta(hours=live.hour, minutes=live.minute)).time())
         
         return {'report': response,
                 'total': {}}
